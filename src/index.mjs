110399ddac51bb9c7ce1e6dd2540527bede8a704
0xean <0xean.eth@gmail.com>
2022-04-24 14:30:34 -0600

    add linter

--- src/index.mjs
+++ src/index.mjs
@@ -1,35 +1,45 @@
-import { ethers } from "ethers";
-import tokenDeployments from '@elasticswap/token/artifacts/deployments.json' assert { type: 'json'};
-import { getAllTokensDepositedEvents, getAllTokensWithdrawnEvents, indexEvents } from "./stakers.mjs";
+import { ethers } from 'ethers';
+import tokenDeployments from '@elasticswap/token/artifacts/deployments.json' assert { type: 'json' };
+import {
+  getAllTokensDepositedEvents,
+  getAllTokensWithdrawnEvents,
+  indexEventsByPoolByUser,
+} from './stakers.mjs';
 
-const EPOCH_LENGTH = 2*7*24*60*60;  // 2 week epochs
+const EPOCH_LENGTH = 2 * 7 * 24 * 60 * 60; // 2 week epochs
 const GENESIS_BLOCK = 13720539;
 const RPC_URL = 'https://api.avax.network/ext/bc/C/rpc';
 
-
-// we need to get all TokensDeposited events and TokensWithdrawn event
 async function main() {
   const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
   const merklePoolsDeployInfo = tokenDeployments[43114][0].contracts.MerklePools;
-  const merklePools = new ethers.Contract(merklePoolsDeployInfo.address, merklePoolsDeployInfo.abi, provider);
-  //const genesisBlock = await provider.getBlock(GENESIS_BLOCK);
+  const merklePools = new ethers.Contract(
+    merklePoolsDeployInfo.address,
+    merklePoolsDeployInfo.abi,
+    provider,
+  );
+  // const genesisBlock = await provider.getBlock(GENESIS_BLOCK);
   const currentBlock = await provider.getBlockNumber();
-  const tokensDepositedEvents = await getAllTokensDepositedEvents(merklePools, GENESIS_BLOCK, currentBlock);
-  const tokensWithdrawnEvents = await getAllTokensWithdrawnEvents(merklePools, GENESIS_BLOCK, currentBlock);
-  
+  const tokensDepositedEvents = await getAllTokensDepositedEvents(
+    merklePools,
+    GENESIS_BLOCK,
+    currentBlock,
+  );
+  const tokensWithdrawnEvents = await getAllTokensWithdrawnEvents(
+    merklePools,
+    GENESIS_BLOCK,
+    currentBlock,
+  );
+
   console.log(`Found ${tokensDepositedEvents.length} TokensDeposited events`);
   console.log(`Found ${tokensWithdrawnEvents.length} TokensWithdrawn events`);
 
   const indexedEvents = {};
-  indexEvents(tokensDepositedEvents, indexedEvents);
-  indexEvents(tokensWithdrawnEvents, indexedEvents);
+  indexEventsByPoolByUser(tokensDepositedEvents, indexedEvents);
+  indexEventsByPoolByUser(tokensWithdrawnEvents, indexedEvents);
 
   // at this point we now have all events indexed by poolId and then user address.
-  
-
 }
-
-
 
 main()
   .then(() => process.exit(0))
--- src/stakers.mjs
+++ src/stakers.mjs
@@ -1,77 +1,91 @@
-import { isTransactionHash } from "../../elasticswap-sdk/src/utils/typeChecks.mjs";
-
 const BLOCK_REQUEST_LIMIT = 2048;
 
-export const getAllTokensDepositedEvents = async (merklePools, genesisBlockNumber, currentBlockNumber ) => {
-  return getAllEvents(merklePools, genesisBlockNumber, currentBlockNumber, merklePools.filters.TokensDeposited());
-}
+export const getAllTokensDepositedEvents = async (
+  merklePools,
+  genesisBlockNumber,
+  currentBlockNumber,
+) =>
+  getAllEvents(
+    merklePools,
+    genesisBlockNumber,
+    currentBlockNumber,
+    merklePools.filters.TokensDeposited(),
+  );
 
-export const getAllTokensWithdrawnEvents = async (merklePools, genesisBlockNumber, currentBlockNumber ) => {
-  return getAllEvents(merklePools, genesisBlockNumber, currentBlockNumber, merklePools.filters.TokensWithdrawn());
-}
+export const getAllTokensWithdrawnEvents = async (
+  merklePools,
+  genesisBlockNumber,
+  currentBlockNumber,
+) =>
+  getAllEvents(
+    merklePools,
+    genesisBlockNumber,
+    currentBlockNumber,
+    merklePools.filters.TokensWithdrawn(),
+  );
 
 export const getAllEvents = async (merklePools, genesisBlockNumber, currentBlockNumber, filter) => {
   let allEvents = [];
   let requestedBlock = genesisBlockNumber;
-  while(requestedBlock < currentBlockNumber) {
+  while (requestedBlock < currentBlockNumber) {
     const nextRequestedBlock = requestedBlock + BLOCK_REQUEST_LIMIT;
     const events = await merklePools.queryFilter(
-      filter, 
-      requestedBlock, 
-      nextRequestedBlock < currentBlockNumber ? nextRequestedBlock : currentBlockNumber
+      filter,
+      requestedBlock,
+      nextRequestedBlock < currentBlockNumber ? nextRequestedBlock : currentBlockNumber,
     );
-    if(events.length > 0) {
+    if (events.length > 0) {
       allEvents = allEvents.concat(events);
       // for testing return here.... TODO: make sure to remove me!
       return allEvents;
     }
     requestedBlock = nextRequestedBlock;
-  }   
+  }
   return allEvents;
-}
+};
 
 /**
  * Process all events in the eventsToIndex array and adds them to eventMapping first
  * indexed by the poolId and then by the user address
- * @param {Array} eventsToIndex 
+ * @param {Array} eventsToIndex
  * @param {Object} eventMapping - mapping of indexed events to add new events to
  * @returns the modified event mapping (same as the original, no cloning is done)
  */
 export const indexEventsByPoolByUser = (eventsToIndex, eventMapping) => {
   eventsToIndex.forEach((event) => {
-    const user = event.args.user;
+    const { user } = event.args;
     const poolId = event.args.poolId.toNumber();
-    const amount = event.args.amount;
-    
+    const { amount } = event.args;
+
     let poolEvents;
-    if(eventMapping[poolId] === undefined) {
+    if (eventMapping[poolId] === undefined) {
       poolEvents = {};
       eventMapping[poolId] = poolEvents;
     } else {
       poolEvents = eventMapping[poolId];
     }
-    
-    let userEvents;;
-    if(poolEvents[user] === undefined ) {
+
+    let userEvents;
+    if (poolEvents[user] === undefined) {
       userEvents = {};
       poolEvents[user] = userEvents;
     } else {
       userEvents = poolEvents[user];
     }
 
-    if(userEvents[event.blockNumber]) {
+    if (userEvents[event.blockNumber]) {
       // currently we assume that users only take one action per block per pool,
-      // just to be safe throw an error if something breaks our assumptions!       
+      // just to be safe throw an error if something breaks our assumptions!
       throw new Error(`Found existing event for user ${user} at block number ${event.blockNumber}`);
     }
-    
+
     userEvents[event.blockNumber] = {
       event: event.event,
       transactionHash: event.transactionHash,
       user,
       poolId,
       amount,
-    }
+    };
   });
   return eventMapping;
-}
+};
